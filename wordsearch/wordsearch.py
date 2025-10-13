a65989f123fbc73b0717ad21acfda072fdbc492b
Linn Oberbeck <lvoberbeck@uchicago.edu>
2025-10-13 09:27:29 -0500

    python program to find words in a letter grid.

--- wordsearch/wordsearch.py
+++ wordsearch/wordsearch.py
@@ -23,6 +23,11 @@ def is_in_bounds(row: int, col: int) -> bool:
     """
     # IMPLEMENTATION HERE
 
+    if row not in range(0,NUM_ROWS) or col not in range(0,NUM_COLS):
+        return False
+    else:
+        return True
+
 
 def check_word_direction(
     puzzle_grid: list[str],
@@ -50,7 +55,38 @@ def check_word_direction(
         the search in the given direction.
         False otherwise.
     """
-    # IMPLEMENTATION HERE
+
+    # create variables out of the inputs to navigate grid
+    starting_row = position[0]
+    starting_col = position[1]
+    
+    direction_row = direction[0]  # change in row per step
+    direction_col = direction[1]  # change in col per step 
+    
+    word_length = len(word)
+
+    i = 0 # keep track of steps in grid
+
+    while i < word_length:
+        new_row = starting_row + (i * direction_row)
+        new_col = starting_col + (i * direction_col)
+
+        # if steps go out of bounds
+        if is_in_bounds(new_row, new_col) == False:
+            return False
+
+        # compare if grid letter and letter in word are equal      
+        position_letter = puzzle_grid[new_row][new_col]
+        word_letter = word[i]
+
+        if position_letter != word_letter:
+            return False
+        
+        # increment count and go to the next letter
+        i += 1
+
+    return True
+
 
 
 def find_word(puzzle_grid: list[str], word: str) -> tuple[int, int]:
@@ -69,6 +105,26 @@ def find_word(puzzle_grid: list[str], word: str) -> tuple[int, int]:
         If word could not be found, return INVALID_WORD constant defined above.
     """
     # IMPLEMENTATION HERE
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+
+    # go through letters in grid
+    for i in range(NUM_ROWS):
+        for j in range(NUM_COLS):
+            current_letter = puzzle_grid[i][j]
+            
+            # start search into diff directions if grid letter is the same as first letter in word
+            if current_letter == word[0]:
+                for d in directions:
+                    success = check_word_direction(puzzle_grid, word, (i,j), d)
+                    if success == True:
+                        return (i,j)
+                    
+    return INVALID_WORD
+
+
+
+
 
 
 def main():
